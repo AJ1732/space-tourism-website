6ce7e0cdada55344ea2ad6841d233ebf63441c62
AJ1732 <ejemeniboi@gmail.com>
2025-09-04 22:30:52 +0100

    feat: update background images

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,47 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+
 import { Navbar } from '@/components/layout'
+
+import { images } from './assets/images'
+import { useRoute } from 'vue-router'
+
+const backgrounds: Record<string, { desktop: string; tablet: string; mobile: string }> = {
+  '/': {
+    desktop: images['home-background-desktop'],
+    tablet: images['home-background-tablet'],
+    mobile: images['home-background-mobile'],
+  },
+  '/destination': {
+    desktop: images['destination-background-desktop'],
+    tablet: images['destination-background-tablet'],
+    mobile: images['destination-background-mobile'],
+  },
+  '/crew': {
+    desktop: images['crew-background-desktop'],
+    tablet: images['crew-background-tablet'],
+    mobile: images['crew-background-mobile'],
+  },
+  '/technology': {
+    desktop: images['technology-background-desktop'],
+    tablet: images['technology-background-tablet'],
+    mobile: images['technology-background-mobile'],
+  },
+}
+
+const route = useRoute()
+const activeBg = computed(() => {
+  return backgrounds[route.path] ?? backgrounds['/']
+})
 </script>
 
 <template>
-  <div class="grid min-h-dvh grid-rows-[auto_1fr]">
+  <div class="relative grid min-h-dvh grid-rows-[auto_1fr]">
+    <picture class="absolute inset-0 -z-[1]">
+      <source :srcset="activeBg['desktop']" media="(min-width: 1024px)" />
+      <source :srcset="activeBg['tablet']" media="(min-width: 768px)" />
+      <img :src="activeBg['mobile']" class="size-full object-cover" />
+    </picture>
     <Navbar />
     <RouterView />
   </div>
--- src/views/home-view.vue
+++ src/views/home-view.vue
@@ -1,16 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { cn } from '@/lib/utils'
+</script>
 
 <template>
   <main class="content-grid py-1600">
     <div
-      class="flex items-center justify-center gap-16 max-lg:flex-col max-lg:text-center lg:justify-between"
+      :class="
+        cn(
+          'flex items-center justify-center gap-16 max-lg:flex-col max-lg:text-center lg:justify-between',
+        )
+      "
     >
       <section class="w-full max-w-lg lg:max-w-[33.75rem]">
         <h2
-          class="font-barlow-condensed text-primary-300 md:text-[1.75rem] font-normal tracking-[0.15rem] md:tracking-[0.25rem] uppercase"
+          class="font-barlow-condensed text-primary-300 font-normal tracking-[0.15rem] uppercase md:text-[1.75rem] md:tracking-[0.25rem]"
         >
           so, you want to travel to <br />
-          <span class="font-bellefair -ml-1.5 text-[5rem] md:text-[9rem] text-white">space</span>
+          <span class="font-bellefair -ml-1.5 text-[5rem] text-white md:text-[9rem]">space</span>
         </h2>
         <p class="font-barlow text-primary-300 leading-[180%] max-md:text-[0.9375rem] lg:text-lg">
           Let&apos;s face it; if you want to go to space, you might as well genuinely go to outer
@@ -19,7 +25,7 @@
         </p>
       </section>
       <button
-        class="font-bellefair text-primary-900 grid size-36 md:size-[17rem] place-content-center rounded-full bg-white text-lg md:text-[2rem] uppercase"
+        class="font-bellefair text-primary-900 grid size-36 place-content-center rounded-full bg-white text-lg uppercase md:size-[17rem] md:text-[2rem]"
       >
         explore
       </button>
