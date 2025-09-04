dad1963c548c089777deeb8fae57d4cc7d36f2c2
AJ1732 <ejemeniboi@gmail.com>
2025-09-04 20:25:20 +0100

    feat: structure images sources into an object

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,6 +1,65 @@
-export const assets = {
+export const images = {
   'res-render-1': '/res-render-1.jpg',
   'res-render-2': '/res-render-2.jpg',
   'res-render-3': '/res-render-3.jpg',
   'res-render-4': '/res-render-4.jpg',
-}
+
+  // CREW IMAGES
+  'crew-background-desktop': '/crew/background-crew-desktop.jpg',
+  'crew-background-mobile': '/crew/background-crew-mobile.jpg',
+  'crew-background-tablet': '/crew/background-crew-tablet.jpg',
+  'crew-anousheh-ansari': {
+    png: '/crew/image-anousheh-ansari.png',
+    webp: '/crew/image-anousheh-ansari.webp',
+  },
+  'crew-douglas-hurley': {
+    png: '/crew/image-douglas-hurley.png',
+    webp: '/crew/image-douglas-hurley.webp',
+  },
+  'crew-mark-shuttleworth': {
+    png: '/crew/image-mark-shuttleworth.png',
+    webp: '/crew/image-mark-shuttleworth.webp',
+  },
+  'crew-victor-glover': {
+    png: '/crew/image-victor-glover.png',
+    webp: '/crew/image-victor-glover.webp',
+  },
+
+  // DESTINATION IMAGES
+  'destination-background-desktop': '/destination/background-destination-desktop.jpg',
+  'destination-background-mobile': '/destination/background-destination-mobile.jpg',
+  'destination-background-tablet': '/destination/background-destination-tablet.jpg',
+  'destination-europa': {
+    png: '/destination/image-europa.png',
+    webp: '/destination/image-europa.webp',
+  },
+  'destination-mars': { png: '/destination/image-mars.png', webp: '/destination/image-mars.webp' },
+  'destination-moon': { png: '/destination/image-moon.png', webp: '/destination/image-moon.webp' },
+  'destination-titan': {
+    png: '/destination/image-titan.png',
+    webp: '/destination/image-titan.webp',
+  },
+
+
+  // HOME IMAGES
+  'home-background-desktop': '/home/background-home-desktop.jpg',
+  'home-background-mobile': '/home/background-home-mobile.jpg',
+  'home-background-tablet': '/home/background-home-tablet.jpg',
+
+  // TECHNOLOGY IMAGES
+  'technology-background-desktop': '/technology/background-technology-desktop.jpg',
+  'technology-background-mobile': '/technology/background-technology-mobile.jpg',
+  'technology-background-tablet': '/technology/background-technology-tablet.jpg',
+  'technology-launch-vehicle': {
+    landscape: '/technology/image-launch-vehicle-landscape.jpg',
+    portrait: '/technology/image-launch-vehicle-portrait.jpg',
+  },
+  'technology-space-capsule': {
+    landscape: '/technology/image-space-capsule-landscape.jpg',
+    portrait: '/technology/image-space-capsule-portrait.jpg',
+  },
+  'technology-spaceport': {
+    landscape: '/technology/image-spaceport-landscape.jpg',
+    portrait: '/technology/image-spaceport-portrait.jpg',
+  },
+} as const
